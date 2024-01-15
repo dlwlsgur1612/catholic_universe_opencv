3592f91214bebd5ffffc7b53f7839bf0d6446c4e
dlwlsgur1612 <dlwlsgur1612@naver.com>
2024-01-15 15:04:42 +0900

    d

---
+++ Python/no2.py
@@ -0,0 +1,18 @@
+def main():
+    # int a;
+    # a = 10;
+    a = 10
+    b = 3.141592
+    print(123456)
+    print("hi my name is Lee", end="ENDSTRING")
+    print(3.141592)
+    print("this is", "python", "class", sep="\t")
+
+    print("variable a is =", a)
+    print("varibale a is {}, b is {}".format(a, b))
+    # f -string
+    print(f"varibale a is {a}, b is {b:.2}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ Python/no4.py
@@ -0,0 +1,11 @@
+def main():
+    s1 = "hello, world"
+    for s in s1:
+        print(s)
+
+    print(s1[0:5])
+    print(s1[7:12])
+    
+
+if __name__ == "__main__":
+    main()
