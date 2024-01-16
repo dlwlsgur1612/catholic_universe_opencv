2ff1139f0bc260c777548e9d3d436aa4d537c64b
dlwlsgur1612 <dlwlsgur1612@naver.com>
2024-01-16 14:26:29 +0900

    d

---
+++ simple_package/simple_node.py
@@ -0,0 +1,28 @@
+import rclpy
+from rclpy.node import Node
+
+
+class Simple_node(Node):
+    def __init__(self):
+        super().__init__("simple_node")
+
+
+def print_hellow():
+    print("first node")
+    print("hellow")
+
+
+def main():
+    rclpy.init()
+    node = Node("simple_node")  # type: ignore
+    node.create_timer(1, print_hellow)
+
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        print("Keyboard stop")
+        node.destroy_node()
+
+
+if __name__ == "__main__":
+    main()
