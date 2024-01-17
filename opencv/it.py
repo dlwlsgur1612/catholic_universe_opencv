ffa25836321b5cb3363831af1e709aa6240429a0
dlwlsgur1612 <dlwlsgur1612@naver.com>
2024-01-17 14:10:15 +0900

    d

---
+++ opencv/it.py
@@ -0,0 +1,19 @@
+import numpy as np
+from npy import show
+
+
+
+def main():
+    array1 = np.zeros((10,12), dtype = np.int8)
+    array2 = np.ones((10, 12), dtype = np.int8)
+    array3 = np.full((10, 12), 126, dtype = np.int8)
+    show("array1", array1)
+    show("array2", array2)
+    show("array3", array3)
+
+
+
+
+
+if __name__ == "__main__":
+    main()
